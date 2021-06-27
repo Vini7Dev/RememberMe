63eb45c77a58d44aea8a27e396f46510f2ceb64f
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-06-27 15:48:13 -0300

    add icon and app global style theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
 import Test from './src';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 const App: React.FC = () => (
-  <View style={styles.container}>
+  <View>
     <Test />
     <StatusBar style="auto" />
   </View>
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,23 @@
+const theme = {
+  baby_blue100: '#00F0FF',
+  baby_blue90: '#00C8F3',
+  baby_blue80: '#21ABFF',
+  baby_blue70: '#009CF5',
+
+  cyan100: '#00576A',
+  cyan90: '#005D96',
+
+  blue: '#001AFF',
+
+  danger100: '#F30F00',
+  danger90: '#990000',
+  danger80: '#6A0000',
+
+  placeholder: '#CECECE',
+  text: '#787878',
+
+  white: '#FFFFFF',
+  white_transparent: 'rgba(100, 100, 100, 0.8)',
+};
+
+export default theme;
