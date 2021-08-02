7756f63f7ec66f821379d7d9007df8b4325554b5
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-08-02 15:44:08 -0300

    Updating readme

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import {
@@ -12,7 +12,64 @@ import {
   Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins';
 
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+
+import { Platform } from 'react-native';
 import Routes from './src/routes';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: 'Here is the notification body',
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      // eslint-disable-next-line no-alert
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    // eslint-disable-next-line no-alert
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  // eslint-disable-next-line consistent-return
+  return token;
+}
 
 const App: React.FC = () => {
   const [fontsLoaded] = useFonts({
@@ -26,6 +83,28 @@ const App: React.FC = () => {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
 
   return (
     <>
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# RememberMe
-Aplicativo para controle de tarefas que se repetem em certas datas ou dias da semana, como consultas médicas e pagamento das contas
+<img src="assets/adaptive-icon.png" width="300" style="border-radius: 50px; margin: 0 auto; display: block" />
+
+# <img src="assets/adaptive-icon.png" width="50"/> &nbsp;&nbsp;Remember Me
+## Controle de tarefas
+Aplicativo para controle de tarefas que se repetem em certas datas ou dias da semana, como consultas médicas e pagamento de contas
+
+### Sobre o desenvolvimento
+- Linguagem: ReactNative com Typescript;
+- Padronização de código: ESLint e Prettier;
+- Documentação e códigos em inglês.
